d28e8617d11ec994911aa187a51ecbd7f242e619
sulimansharif <sharifsuliman1@gmail.com>
2019-02-23 02:06:49 -0600

    MASTER Added to the editable molecule function, and first pass at R group library building

---
+++ fragmentation/editable_molecule.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#
+# Runs R Group Converter for Library Creation
+#
+# ----------------------------------------------------------
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem.Draw import IPythonConsole
+from rdkit.Chem.Draw import MolDrawing, DrawingOptions
+
+R_GROUP_LIST = ["Br", "Cl", "F", "O", "O=C", "OC=O", "N"]
+
+class RGroupMolObject(object):
+    """
+
+    This class is used to enumerate an object
+    """
+
+    R_GROUP_LIST = ["Br", "Cl", "F", "O", "O=C", "OC=O", "N"]
+
+    __version_parser__ = 1.0
+    __allow_update__ = False
+
+    def __init__(self, molecule):
+        self.molecule = molecule
+        self.original_smiles = Chem.MolToSmiles(self.molecule)
+        self.validate_molecule()
+
+    def validate_molecule(self):
+        """
+
+        This method takes the smiles string and runs through the validation check of a smiles string.
+
+        """
+
+        # Use MolVS to validate the smiles to make sure enumeration and r group connections are correct
+        # at least in the 1D Format.
+        from molvs import validate_smiles
+        try:
+            validate_smiles(self.original_smiles)
+        except RaiseMoleculeError:
+            print ("Not a Valid Smiles, Please check the formatting: %s" % self.original_smiles)
+
+    def r_group_enumerator(self):
+        """
+
+        This method will run through the R_Group List to generate smiles strings.
+
+        """
+        Chem.ReplaceSubstructs(self.molecule)
+
+
+
+if __name__ == "__main__":
+        start_mol = Chem.MolFromSmiles('c1cc(CCCO)ccc1')
+        truncate = Chem.DeleteSubstructs(start_mol, Chem.MolFromSmiles('O'))
+        molecule = RGroupMolObject(Chem.MolFromSmiles(i))
+        molecule.validate_molecule()
+
+
+        # combo = Chem.CombineMols(truncate, mod)
+        # Chem.MolToSmiles(combo)
+        # edcombo = Chem.EditableMol(combo)
+        # edcombo.AddBond(3, 9, order=Chem.rdchem.BondType.SINGLE)
+        # back = edcombo.GetMol()
+        # print(Chem.MolToSmiles(back))
