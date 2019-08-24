3fde2e3d8c0b539b052e6367e394ca7717c1e5f6
sulimansharif <sharifsuliman1@gmail.com>
2019-08-24 13:42:52 -0500

    ss/MASTER fixing tests for file handler

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -39,17 +39,53 @@ def test_file_production():
     """
 
     scaffold_molecule = Cocktail([Chem.MolFromSmiles('c1cc(CCCO)ccc1'), Chem.MolFromSmiles('c1cc(CCCBr)ccc1')])
-    patterns_found = scaffold_molecule.detect_functional_groups()
-    modified_molecules = scaffold_molecule.shake(patterns_found=patterns_found)
+    modified_molecules = scaffold_molecule.shake()
     FileWriter("tests/test", modified_molecules, "sdf")
     FileWriter("tests/test", modified_molecules, "txt")
+    FileWriter("tests/test", modified_molecules, "alc")
+    FileWriter("tests/test", modified_molecules, "cdxml")
+    FileWriter("tests/test", modified_molecules, "cerius")
+    FileWriter("tests/test", modified_molecules, "charmm")
+    FileWriter("tests/test", modified_molecules, "cif")
+    FileWriter("tests/test", modified_molecules, "cml")
+    FileWriter("tests/test", modified_molecules, "ctx")
+    FileWriter("tests/test", modified_molecules, "gjf")
+    FileWriter("tests/test", modified_molecules, "gromacs")
+    FileWriter("tests/test", modified_molecules, "hyperchem")
+    FileWriter("tests/test", modified_molecules, "jme")
+    FileWriter("tests/test", modified_molecules, "maestro")
+    FileWriter("tests/test", modified_molecules, "mol")
+    FileWriter("tests/test", modified_molecules, "mol2")
+    FileWriter("tests/test", modified_molecules, "pdb")
+    FileWriter("tests/test", modified_molecules, "mrv")
+    FileWriter("tests/test", modified_molecules, "sdf3000")
+    FileWriter("tests/test", modified_molecules, "sln")
+    FileWriter("tests/test", modified_molecules, "xyz")
 
     from pathlib import Path
     dir_path = os.path.dirname(os.path.realpath(__file__))
 
     assert Path(dir_path + "/test.sdf").is_file() == True
     assert Path(dir_path + "/test.txt").is_file() == True
-
+    assert Path(dir_path + "/test.alc").is_file() == True
+    assert Path(dir_path + "/test.cdxml").is_file() == True
+    assert Path(dir_path + "/test.cerius").is_file() == True
+    assert Path(dir_path + "/test.charmm").is_file() == True
+    assert Path(dir_path + "/test.cif").is_file() == True
+    assert Path(dir_path + "/test.cml").is_file() == True
+    assert Path(dir_path + "/test.ctx").is_file() == True
+    assert Path(dir_path + "/test.gjf").is_file() == True
+    assert Path(dir_path + "/test.gromacs").is_file() == True
+    assert Path(dir_path + "/test.hyperchem").is_file() == True
+    assert Path(dir_path + "/test.jme").is_file() == True
+    assert Path(dir_path + "/test.maestro").is_file() == True
+    assert Path(dir_path + "/test.mol").is_file() == True
+    assert Path(dir_path + "/test.mol2").is_file() == True
+    assert Path(dir_path + "/test.pdb").is_file() == True
+    assert Path(dir_path + "/test.mrv").is_file() == True
+    assert Path(dir_path + "/test.sdf3000").is_file() == True
+    assert Path(dir_path + "/test.sln").is_file() == True
+    assert Path(dir_path + "/test.xyz").is_file() == True
 
 def test_file_parser():
 
