0a062d146fd073950888fc7e637429a72f6475c3
Sulstice <sharifsuliman1@gmail.com>
2020-04-13 21:41:58 -0500

    ss/MASTER added a new version number

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ ENTRY_POINTS = {
 # ----
 setup(
     name="cocktail_shaker",
-    version="1.1.2",
+    version="1.1.3",
     packages=['cocktail_shaker'],
     license='MIT',
     author="Suliman Sharif",
